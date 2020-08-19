f3c5231acc5b02372ac58fcdc42e27ce01d0ce2e
Radek Zikmund <r.zikmund.rz@gmail.com>
2020-08-19 21:45:22 +0200

    Add Watch-File function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -55,16 +55,16 @@ function InitializeModules
 
 function LinuxSetup
 {
-    $Env:EDITOR="$(which runemacs) -t -a vim"
-    $Env:VISUAL="$(which runemacs) -c -a 'emacs'"
-
-    $Env:SUDO_EDITOR="$(which emacsclient) -t -a vim"
-
     Add-Path ~/bin
     Add-Path ~/.config/composer/vendor/bin/
     Add-Path ~/.gem/ruby/2.7.0/bin/
     Add-Path ~/.dotnet/tools/
     Add-Path /usr/local/texlive/2020/bin/x86_64-linux
+
+    $Env:EDITOR="$(which runemacs) -t -a vim"
+    $Env:VISUAL="$(which runemacs) -c -a 'emacs'"
+
+    $Env:SUDO_EDITOR="$(which emacsclient) -t -a vim"
 }
 
 
@@ -82,4 +82,46 @@ if ($IsLinux)
 if (Test-Path -PathType Leaf $PSScriptRoot/local.ps1)
 {
     . $PSScriptRoot/local.ps1
+}
+
+function Watch-File
+{
+    param(
+        # Filter for file names
+        [Parameter(Mandatory = $true)]
+        [string[]] $Filters,
+        # Script to call on changed files.
+        [Parameter(Mandatory = $true)]
+        [scriptblock] $ScriptBlock,
+        # Filter for file system events
+        [System.IO.NotifyFilters] $NotifyFilter = $null,
+        # Directory to watch. Defaults to current directory.
+        [string] $Directory = $pwd
+    )
+
+    $watcher = New-Object System.IO.FileSystemWatcher
+    $watcher.Path = $Directory
+    $Filters | ForEach-Object { $watcher.Filters.Add($_) }
+    $watcher.IncludeSubdirectories = $true
+    if ($NotifyFilter) { $watcher.NotifyFilter = $NotifyFilter }
+
+    # we need to use MessageData to pass arguments to the action block
+    Register-ObjectEvent $watcher "Changed" -MessageData $ScriptBlock -Action {
+        Invoke-Command $Event.MessageData | Out-Host
+    } > $null
+
+    try
+    {
+        # start watching
+        Write-Host "Watching..."
+        $watcher.EnableRaisingEvents = $true
+        $watcher
+        while ($true) { Start-Sleep -Milliseconds 200 }
+    }
+    finally
+    {
+        # cleanup
+        $watcher.EnableRaisingEvents = $false
+        $watcher.Dispose()
+    }
 }
