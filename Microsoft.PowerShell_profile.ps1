5b9ca1b76256aba3ebff99058504028922f6b50f
Radek Zikmund <radek.zikmund@bisimulations.com>
2020-12-14 15:41:20 +0100

    Add dotnet argument completer

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -205,3 +205,11 @@ function Watch-Command
 
 . $PSScriptRoot/Trace-Dotnet.ps1
 . $PSScriptRoot/Use-DotnetRoot.ps1
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+     param($commandName, $wordToComplete, $cursorPosition)
+         dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+         }
+ }
