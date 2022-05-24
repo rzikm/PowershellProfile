824ae32e114e68b623ac47550779c7fd26a9dd9e
Radek Zikmund <radekzikmund@microsoft.com>
2022-05-24 14:14:14 +0200

    Improve Install-Dotnet on Linux

--- Install-Dotnet.ps1
+++ Install-Dotnet.ps1
@@ -11,24 +11,42 @@ function Install-Dotnet {
     )
 
     $installScript = New-TemporaryFile
-    $installScript = Rename-Item -Path $installScript -NewName ($installScript.BaseName + ".ps1") -PassThru
+    if ($IsWindows) {
+        $installScript = Rename-Item -Path $installScript -NewName ($installScript.BaseName + ".ps1") -PassThru
+    }
+    else {
+        $installScript = Rename-Item -Path $installScript -NewName ($installScript.BaseName + ".sh") -PassThru
+    }
 
     $ProgressPreference = 'SilentlyContinue' # Don't display the console progress UI - it's a huge perf hit
     $dotnetInstallScriptVersion = "v1"
-    $uri = "https://dotnet.microsoft.com/download/dotnet/scripts/$dotnetInstallScriptVersion/dotnet-install.ps1"
+
+    if ($IsWindows) {
+        $uri = "https://dotnet.microsoft.com/download/dotnet/scripts/$dotnetInstallScriptVersion/dotnet-install.ps1"
+    }
+    else {
+        $uri = "https://dotnet.microsoft.com/download/dotnet/scripts/$dotnetInstallScriptVersion/dotnet-install.sh"
+    }
 
     Invoke-WebRequest $uri -OutFile $installScript
 
-    $parameters = @{
-        Version    = $Version
-        InstallDir = $InstallDir
-    }
+    $parameters = @(
+        "-Version", $Version
+        "-InstallDir", $InstallDir
+    )
 
     if ($DryRun) {
-        $parameters += @{ DryRun = $true }
+        $parameters += @(
+            "-DryRun"
+        )
     }
 
-    & $installScript @parameters
+    if ($IsWindows) {
+        pwsh $installScript @parameters
+    }
+    else {
+        bash $installScript @parameters
+    }
 
     Remove-Item $installScript
 }
