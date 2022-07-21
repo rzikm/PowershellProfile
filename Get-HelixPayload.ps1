d9faceef28127c3b69fc789132b750c079dc67c7
Radek Zikmund <radekzikmund@microsoft.com>
2022-07-21 18:10:49 +0200

    Get HelixPayload by console log url

--- Get-HelixPayload.ps1
+++ Get-HelixPayload.ps1
@@ -3,17 +3,32 @@ function Get-HelixPayload {
     [Alias('ghp')]
     param(
         # Helix job id
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ParameterSetName = "JobAndWorkItem")]
         [string] $JobId,
 
         # Work item id
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ParameterSetName = "JobAndWorkItem")]
         [string] $WorkItemId,
+
+        # Link to the console output
+        [Parameter(Mandatory, ParameterSetName = "ConsoleUri")]
+        [string] $ConsoleUri,
 
         # Path where the payload should be extracted
         [Parameter(Mandatory)]
         [string] $OutDir
     )
+
+    if ($PSCmdlet.ParameterSetName -eq "ConsoleUri") {
+        $content = Invoke-RestMethod -Uri $ConsoleUri
+        if ($content -match "Console log: '([^']+)' from job ([^ ]+) ") {
+            $WorkItemId = $Matches[1]
+            $JobId = $Matches[2]
+        }
+        else {
+            Write-Error "Unable to retrieve WorkItemId and JobId from the console log"
+        }
+    }
 
     # download payload
     runfo get-helix-payload -j $JobId -w $WorkItemId -o $OutDir
